8ad4467d8a2453a8505601596eefbd3d26bf4795
Satyam-Singh-4 <satyam7619patel@gmail.com>
2022-11-07 17:25:52 +0530

    first commit

--- controller/fileController.js
+++ controller/fileController.js
@@ -1,10 +1,10 @@
 const fs = require("fs").promises;
 
-const createFile = (req, res) => {
+const createFile = async(req, res) => {
   try {
     const { fname, data } = req.body;
 
-    const file = fs.writeFile(fname, data);
+    const file = await fs.writeFile(fname, data);
     res.status(200).json({
       response: file,
       message: "file created succssefully",
@@ -16,6 +16,23 @@ const createFile = (req, res) => {
     });
   }
 };
+
+const readFile1=async(req,res)=>{
+  try {
+    const {path}=req.body;
+  const resp=await fs.readFile(path);
+  res.status(200).json({
+    response:resp.toString(),
+    message:"succseefully readed"
+  })
+  } catch (error) {
+    res.status(400).json({
+      message:"not readed",
+      response:error
+    })
+  }
+}
 module.exports = {
   createFile,
+  readFile1
 };
--- router/router.js
+++ router/router.js
@@ -2,5 +2,6 @@ const router=require('express').Router()
 const controller=require('../controller/fileController')
 
 router.post('/create',controller.createFile)
+router.post('/read',controller.readFile1)
 
 module.exports=router
