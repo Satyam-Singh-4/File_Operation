30b2f431264edca0a8630ef346e2711bc168493b
Satyam-Singh-4 <satyam7619patel@gmail.com>
2022-11-08 09:54:39 +0530

    added_new code

--- controller/fileController.js
+++ controller/fileController.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 
-const createFile = async(req, res) => {
+const createFile = async (req, res) => {
   try {
     const { fname, data } = req.body;
 
@@ -17,39 +17,56 @@ const createFile = async(req, res) => {
   }
 };
 
-const readFile1=async(req,res)=>{
+const readFile1 = async (req, res) => {
   try {
-    const {path}=req.body;
-  const resp=await fs.readFile(path);
-  res.status(200).json({
-    response:resp.toString(),
-    message:"successfully readied"
-  })
+    const { path } = req.body;
+    const resp = await fs.readFile(path);
+    res.status(200).json({
+      response: resp.toString(),
+      message: "successfully readied",
+    });
   } catch (error) {
     res.status(400).json({
-      message:"not readied",
-      response:error
-    })
+      message: "not readied",
+      response: error,
+    });
   }
-}
+};
 //Shifting file from one folder to another folder
-const shiftFile=async(req,res)=>{
+const shiftFile = async (req, res) => {
   try {
-    const{source,destination}=req.body;
-     const resp=await fs.rename(source, destination);
-     res.status(200).json({
-      response:resp,
-      message:"successfully shifted"
-     })
+    const { source, destination } = req.body;
+    const resp = await fs.rename(source, destination);
+    res.status(200).json({
+      response: resp,
+      message: "successfully shifted",
+    });
   } catch (error) {
     res.status(400).json({
-      response:error,
-      message:"unable to shift this file"
-    })
+      response: error,
+      message: "unable to shift this file",
+    });
   }
-}
+};
+//delete file
+const removeFile = async (req, res) => {
+  try {
+    const { path } = req.body;
+    const resp = await fs.unlink(path);
+    res.status(200).json({
+      response: resp,
+      message: "file deleted successfully ",
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error,
+      message: "unable to delete this file",
+    });
+  }
+};
 module.exports = {
   createFile,
   readFile1,
-  shiftFile
+  shiftFile,
+  removeFile,
 };
